6059f7ac1f594dc407489c56056826e70d0dc490
Jeremiah Nicholls <jnicholls@reputation.com>
2021-09-24 12:53:21 -0600

    create_spiro(a,b,c,) returns array

--- src/SpiroFunc.js
+++ src/SpiroFunc.js
@@ -1,11 +1,26 @@
+
 function create_spiro(a, b, d){
     let dt = 0.01;
     let t = 0;
-    //let pts = [];
-    while t < (2*math.pi*b /math.gcd(a, b)){
-        t += dt
-        x = (a - b) * math.cos(t) + d * math.cos((a - b)/b * t)
-        y = (a - b) * math.sin(t) - d * math.sin((a - b)/b * t)
-        pts.append((x, y))}
+    const pts = [];
+    while (t < 2*Math.pi*b / gCD(a, b)){
+        t += dt;
+        x = (a - b) * Math.cos(t) + d * Math.cos((a - b)/b * t);
+        y = (a - b) * Math.sin(t) - d * Math.sin((a - b)/b * t);
+        pts.concat((x, y));
+    }
     return pts
 }
+
+
+function gCD (x, y){
+   
+  x = Math.abs(x);
+  y = Math.abs(y);
+  while(y) {
+    var t = y;
+    y = x % y;
+    x = t;
+  }
+  return x;
+}   
